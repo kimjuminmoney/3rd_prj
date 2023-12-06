157a9eb8af65a48aa25980de59286d3dbdbde6a6
user <user@DESKTOP-COJ9858>
2023-12-06 18:31:02 +0900

    test

--- 3rd_prj/src/main/java/kr/co/daitdayoung/instructor/DAO/MyClassDAO.java
+++ 3rd_prj/src/main/java/kr/co/daitdayoung/instructor/DAO/MyClassDAO.java
@@ -1,8 +1,24 @@
 package kr.co.daitdayoung.instructor.DAO;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import kr.co.daitdayoung.dao.MyBatisHandler;
+import kr.co.daitdayoung.instructor.domain.CourseDomain;
+
 public class MyClassDAO {
 
-	public void SearchMyCourses() {
+	public void SearchMyCourses(String insId) {
+		MyBatisHandler mbh= MyBatisHandler.getInstance();
 		
+		SqlSession ss=mbh.getMyBatisHandler(false);
+		
+		List<CourseDomain> cd=ss.selectList("kr.co.daitdayoung.instructor.selectMyCourses",insId);
+		System.out.println(cd);
+	}
+	
+	public static void main(String[] args) {
+		new MyClassDAO().SearchMyCourses("ins1");
 	}
 }
