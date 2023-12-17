bc392b34db764a197da9c2498759d8530fd0f6c7
DESKTOP-COJ9858\user <dkrnlqkqn586@naver.com>
2023-12-17 16:13:53 +0900

    김주민

--- 3rd_prj/src/main/java/kr/co/daitdayoung/index/controller/CoursesDetailController.java
+++ 3rd_prj/src/main/java/kr/co/daitdayoung/index/controller/CoursesDetailController.java
@@ -13,28 +13,8 @@ import kr.co.daitdayoung.index.domain.CoursesDomain;
 import kr.co.daitdayoung.index.domain.IndexDomain;
 import kr.co.daitdayoung.index.service.CoursesService;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class CoursesDetailController {
-	
-	@Autowired
-	private CoursesService cs;
-	
-	
-//	@RequestMapping(value = "/courses.do", method = RequestMethod.GET)
-//	public String courses(Model model) {
-//
-//		List<CoursesDomain> LCList = cs.searchLeftCat();
-//		
-//		model.addAttribute("LCList",LCList);
-//		
-//		return "/courses/courses";
-//	}
-//	
-	
-	
 	
 	@RequestMapping(value = "/courses_detail.do", method = RequestMethod.GET)
 	public String coursesDetail(String couCode,Model model) {
